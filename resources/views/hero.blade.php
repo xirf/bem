a1da71b7bdb8e6892f5e9e5d4d71d0242e79166d
mbgigs03 <mbgigihbaskoroashari@gmail.com>
2024-01-20 13:32:30 +0700

    Hero-Section

---
+++ resources/views/hero.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" />
+  </head>
+  <body>
+    <!-- Hero Section -->
+    <div class="hero">
+      <img class="background" src="{{ asset('/assets/img/Background.png') }}" />
+      <div class="text">
+        <div class="tagline">
+          <p class="menyatu-dalam-aksi">Menyatu Dalam Aksi, <br />membangun Masa Depan Bersama</p>
+        </div>
+        <div class="subtagline">
+          <div class="overlap-group">
+            <div class="rectangle"></div>
+            <div class="div"></div>
+            <p class="bersama-mahasiswa">Bersama Mahasiswa, Wujudkan Perubahan Menuju Kampus Yang Inspiratif.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Of Hero Section -->
+  </body>
+</html>
