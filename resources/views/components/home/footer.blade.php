36856389f1c7006a5f0ddda78f3b417d7f2aba8d
xirf <itsandka@gmail.com>
2024-01-21 18:13:59 +0700

    refactor kode sementara

---
+++ resources/views/components/home/footer.blade.php
@@ -0,0 +1,111 @@
+<style>
+    .rata-kiri {
+        text-align: left;
+    }
+</style>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+
+    .footer {
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+        text-align: center;
+    }
+
+    .footer-section {
+        display: flex;
+        margin-top: 10px;
+    }
+
+    .footer-section .medsos {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .footer-section .medsos a {
+        margin: 0 10px;
+    }
+
+    .footer-section div {
+        flex: 1;
+        padding: 0 10px;
+    }
+
+    h3 {
+        color: #f3c623;
+        /* Warna kuning */
+    }
+
+    p {
+        font-size: 14px;
+    }
+
+    a {
+        color: #fff;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    h4 {
+        border-bottom: 2px solid #f3c623;
+        /* Warna kuning */
+        display: inline-block;
+        padding-bottom: 5px;
+        /* Jarak antara teks dan garis bawah */
+    }
+</style>
+
+
+<!-- Konten halaman di sini -->
+
+<div class="footer">
+    <div class="footer-section">
+        <div>
+            <img src="logo 2.png" alt="" width="200px">
+            <p>DIREKTORAT
+                KEMAHASISWAAN
+            </p>
+        </div>
+        <div>
+            <h4>KONTAK</h4>
+            <p class="rata-kiri">Telp. (0352) 481124, <br>Fax : (0352) 461796,
+                <br>humas@umpo.ac.id , akademik@umpo.ac.id
+            </p>
+        </div>
+        <div>
+            <h4>ALAMAT</h4>
+            <p class="rata-kiri">Jl. Budi Utomo No.10, Ronowijayan, Kec. Siman, Kabupaten Ponorogo, Jawa Timur 63471,<br>Indonesia</p>
+        </div>
+        <div>
+            <h4>SOCIAL MEDIA</h4>
+            <div class="footer-section">
+                <div class="medsos">
+                    <a href="#">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="https://www.instagram.com/bem_umpo/">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-tiktok"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p>&copy; 2024 BEM Umpo. All Rights Reserved. | <a href="#">Kebijakan Privasi</a></p>
+</div>
---
+++ resources/views/components/home/hero.blade.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" />
+
+<div class="hero">
+  <img class="background" src="{{ asset('/assets/img/Background.png') }}" />
+  <div class="text">
+    <div class="tagline">
+      <p class="menyatu-dalam-aksi">Menyatu Dalam Aksi, <br />membangun Masa Depan Bersama</p>
+    </div>
+    <div class="subtagline">
+      <div class="overlap-group">
+        <div class="rectangle"></div>
+        <div class="div"></div>
+        <p class="bersama-mahasiswa">Bersama Mahasiswa, Wujudkan Perubahan Menuju Kampus Yang Inspiratif.</p>
+      </div>
+    </div>
+  </div>
+</div>
