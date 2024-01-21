a38213c6f89fc4ab83627cd0b87d90dfa691f4fb
xirf <itsandka@gmail.com>
2024-01-21 18:21:17 +0700

    fix hero

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,17 +1,15 @@
-<link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" />
-
-<div class="hero">
-  <img class="background" src="{{ asset('/assets/img/Background.png') }}" />
-  <div class="text">
-    <div class="tagline">
-      <p class="menyatu-dalam-aksi">Menyatu Dalam Aksi, <br />membangun Masa Depan Bersama</p>
+<div class="hero w-full min-"
+    style="background-image: url('{{ asset('/assets/img/Background.png') }}'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <div class="text">
+        <div class="tagline">
+            <p class="menyatu-dalam-aksi">Menyatu Dalam Aksi, <br />membangun Masa Depan Bersama</p>
+        </div>
+        <div class="subtagline">
+            <div class="overlap-group">
+                <div class="rectangle"></div>
+                <div class="div"></div>
+                <p class="bersama-mahasiswa">Bersama Mahasiswa, Wujudkan Perubahan Menuju Kampus Yang Inspiratif.</p>
+            </div>
+        </div>
     </div>
-    <div class="subtagline">
-      <div class="overlap-group">
-        <div class="rectangle"></div>
-        <div class="div"></div>
-        <p class="bersama-mahasiswa">Bersama Mahasiswa, Wujudkan Perubahan Menuju Kampus Yang Inspiratif.</p>
-      </div>
-    </div>
-  </div>
 </div>
