73eb4320a467dbb7606fd4197c9ae17c1ffb871d
xirf <itsandka@gmail.com>
2024-01-29 22:52:36 +0700

    Update profil.blade.php layout

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.default>
-    <div class="max-w-screen-lg w-full m-auto border-2 border-gray-100 min-h-screen p-8 flex flex-col gap-10">
+    <div class="max-w-screen-lg w-full m-auto border-2 border-gray-100 py-20 p-8 flex flex-col gap-10">
         <h1 class="text-4xl font-bold relative">Badan Eksekutif Mahasiswa <span
                 class="absolute -bottom-2 left-0 w-32 h-1 bg-orange-300"></span></h1>
-        <main>
+        <main class="grid gap-4">
             <article>
                 <p>Badan Eksekutif Mahasiswa (BEM) adalah organisasi mahasiswa intra kampus yang merupakan lembaga
                     eksekutif di tingkat Universitas. BEM Umpo bertugas untuk merencanakan dan mengorganisasikan
@@ -11,7 +11,7 @@
             </article>
 
             <article>
-                <h2>Tentang BEM</h2>
+                <h2 class="text-xl font-bold">Tentang BEM</h2>
                 <p>BEM merupakan lembaga mahasiswa yang memiliki peran penting dalam mewakili dan memperjuangkan
                     kepentingan mahasiswa. Mulai dari penyelenggaraan acara, advokasi isu-isu mahasiswa, hingga
                     kerjasama dengan pihak kampus, BEM berperan aktif dalam meningkatkan kualitas kehidupan mahasiswa.
@@ -19,27 +19,27 @@
             </article>
 
             <article>
-                <h2>Pengurus BEM</h2>
+                <h2 class="text-xl font-bold">Pengurus BEM</h2>
                 <p>Kenali lebih dekat dengan anggota pengurus BEM, termasuk Presiden BEM, Wakil Presiden, dan anggota
                     departemen yang bertanggung jawab atas berbagai bidang, seperti akademik, sosial, dan kebudayaan.
                     Mereka adalah para pemimpin yang bekerja keras untuk mewujudkan visi dan misi BEM.</p>
             </article>
 
             <article>
-                <h2>Program Unggulan</h2>
+                <h2 class="text-xl font-bold">Program Unggulan</h2>
                 <p>Temukan program-program unggulan yang diinisiasi oleh BEM untuk meningkatkan kesejahteraan dan
                     pengalaman belajar mahasiswa. Mulai dari seminar, workshop, hingga proyek sosial, BEM berkomitmen
                     untuk memberikan kontribusi positif bagi seluruh mahasiswa.</p>
             </article>
 
             <article>
-                <h2>Partisipasi Mahasiswa</h2>
+                <h2 class="text-xl font-bold">Partisipasi Mahasiswa</h2>
                 <p>BEM mendorong partisipasi mahasiswa dalam pengambilan keputusan dan pelaksanaan berbagai kegiatan.
                     Dengan bergabung dan berkontribusi di BEM, mahasiswa dapat turut serta dalam pembentukan kebijakan
                     dan menciptakan lingkungan kampus yang lebih baik.</p>
             </article>
 
         </main>
-
     </div>
+    <x-home.footer />
 </x-layouts.default>
